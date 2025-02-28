1689a878f63e31ec496577d306f8c8802ea37df9
Me1k0r <runner-me1k0r@proton.me>
2025-02-28 21:39:08 +0100

    new: initial commit 2

---
+++ init.lua
@@ -0,0 +1,8 @@
+local home = os.getenv 'HOME'
+package.path = package.path .. ';' .. home .. '/.config/nvim/lua?.lua'
+vim.opt.runtimepath:append(home .. '/.config/nvim/')
+
+require("user.options")
+require("user.lazy")
+require("user.autocmd")
+require("user.mappings")
