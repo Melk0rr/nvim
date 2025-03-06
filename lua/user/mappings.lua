30dcdaf28943a1dec3b83661edb3abe2c87a974b
Me1k0r <runner-me1k0r@proton.me>
2025-03-06 13:54:57 +0100

    edit: mappings - formatting + new file
    New file mapping + some quotes consistency

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -5,50 +5,51 @@ end
 
 -- NOTE: Common
 map('', 'Y', 'y$', opts("Yank whole line"))
-map('n', '<leader;', 'mqA;<esc>`q', opts("Semicolon at end of line"))
-map('n', '<leader>h', '<cmd>noh<cr>', opts("No search highlight"))
-map('n', '<leader>qq', '<cmd>wqa!<cr>', opts("Save and quit"))
-map('n', '<C-a>', 'goVG', opts("Select all"))
+map('n', "<leader;", "mqA;<esc>`q", opts("Semicolon at end of line"))
+map('n', "<leader>h", "<cmd>noh<cr>", opts("No search highlight"))
+map('n', "<leader>qq", "<cmd>wqa!<cr>", opts("Save and quit"))
+map('n', "<C-a>", "goVG", opts("Select all"))
+map('n', "<leader>nf", "<cmd>enew<cr>", opts("New file"))
 
 -- NOTE: Press jk fast to exit insert mode
-map('i', 'jk', '<ESC>', opts("jk to exit insert mode"))
-map('i', 'kj', '<ESC>', opts("kj to exit insert mode"))
+map('i', "jk", "<ESC>", opts("jk to exit insert mode"))
+map('i', "kj", "<ESC>", opts("kj to exit insert mode"))
 
 -- NOTE: Window navigation
-map('n', '<C-h>', '<C-w>h', opts("Go to left window"))
-map('n', '<C-j>', '<C-w>j', opts("Go to lower window"))
-map('n', '<C-k>', '<C-w>k', opts("Go to upper window"))
-map('n', '<C-l>', '<C-w>l', opts("Go to right window"))
-map('t', '<C-h>', [[<C-\><C-n><C-w>h]], opts("Go to left terminal"))
-map('t', '<C-j>', [[<C-\><C-n><C-w>j]], opts("Go to lower terminal"))
-map('t', '<C-k>', [[<C-\><C-n><C-w>k]], opts("Go to upper terminal"))
-map('t', '<C-l>', [[<C-\><C-n><C-w>l]], opts("Go to right terminal"))
+map('n', "<C-h>", "<C-w>h", opts("Go to left window"))
+map('n', "<C-j>", "<C-w>j", opts("Go to lower window"))
+map('n', "<C-k>", "<C-w>k", opts("Go to upper window"))
+map('n', "<C-l>", "<C-w>l", opts("Go to right window"))
+map('t', "<C-h>", [[<C-\><C-n><C-w>h]], opts("Go to left terminal"))
+map('t', "<C-j>", [[<C-\><C-n><C-w>j]], opts("Go to lower terminal"))
+map('t', "<C-k>", [[<C-\><C-n><C-w>k]], opts("Go to upper terminal"))
+map('t', "<C-l>", [[<C-\><C-n><C-w>l]], opts("Go to right terminal"))
 
 -- NOTE: Buffer navigation
-map('n', '<Tab>', '<cmd>bnext<cr>', opts("Next buffer"))
-map('n', '<S-Tab>', '<cmd>bprevious<cr>', opts("Previous buffer"))
-map('n', '<leader>bd', "<cmd>lua require('snacks').bufdelete()<cr>", opts("Delete buffer"))
-map('n', '<leader>bdo', "<cmd>lua require('snacks').bufdelete.other()<cr>", opts("Delete other buffer"))
-map('n', '<leader>bde', '<cmd>%bd|e#<cr>', opts("Close all buffers except the current one"))
+map('n', "<Tab>", "<cmd>bnext<cr>", opts("Next buffer"))
+map('n', "<S-Tab>", "<cmd>bprevious<cr>", opts("Previous buffer"))
+map('n', "<leader>bd", "<cmd>lua require('snacks').bufdelete()<cr>", opts("Delete buffer"))
+map('n', "<leader>bdo", "<cmd>lua require('snacks').bufdelete.other()<cr>", opts("Delete other buffer"))
+map('n', "<leader>bde", "<cmd>%bd|e#<cr>", opts("Close all buffers except the current one"))
 
 -- NOTE: Stay in indent mode
-map('v', '<', '<gv', opts("Unindent"))
-map('v', '>', '>gv', opts("Indent"))
+map('v', '<', "<gv", opts("Unindent"))
+map('v', '>', ">gv", opts("Indent"))
 
 -- NOTE: Move lines
-map('n', '<A-j>', ':m .+1<cr>==', opts("Move line down"))
-map('n', '<A-k>', ':m .-2<cr>==', opts("Move line up"))
-map('v', '<A-j>', ":m '>+1<cr>gv=gv", opts("Move selection down"))
-map('v', '<A-k>', ":m '<-2<cr>gv=gv", opts("Move selection up"))
+map('n', "<A-j>", ":m .+1<cr>==", opts("Move line down"))
+map('n', "<A-k>", ":m .-2<cr>==", opts("Move line up"))
+map('v', "<A-j>", ":m '>+1<cr>gv=gv", opts("Move selection down"))
+map('v', "<A-k>", ":m '<-2<cr>gv=gv", opts("Move selection up"))
 
 -- NOTE: Debugging
-map('n', "<leader>dc", '<cmd>lua require("dap").continue()<cr>', opts("Debugger continue"))
-map('n', "<leader>dj", '<cmd>lua require("dap").step_over()<cr>', opts("Debugger step over"))
-map('n', "<leader>dk", '<cmd>lua require("dap").step_out()<cr>', opts("Debugger step out"))
-map('n', "<leader>dl", '<cmd>lua require("dap").step_into()<cr>', opts("Debugger step into"))
-map('n', "<leader>db", '<cmd>lua require("dap").toggle_breakpoint()<cr>', opts("Debugger toggle breakpoint"))
-map('n', "<leader>dt", '<cmd>lua vim.cmd("RustLsp testables")<cr>', opts("Debugger run rust tests"))
-map('n', "<leader>dx", '<cmd>lua require("dap").terminate()<cr><cmd>lua require("dapui").close()<cr>', opts("Debugger terminate"))
+map('n', "<leader>dc", "<cmd>lua require('dap').continue()<cr>", opts("Debugger continue"))
+map('n', "<leader>dj", "<cmd>lua require('dap').step_over()<cr>", opts("Debugger step over"))
+map('n', "<leader>dk", "<cmd>lua require('dap').step_out()<cr>", opts("Debugger step out"))
+map('n', "<leader>dl", "<cmd>lua require('dap').step_into()<cr>", opts("Debugger step into"))
+map('n', "<leader>db", "<cmd>lua require('dap').toggle_breakpoint()<cr>", opts("Debugger toggle breakpoint"))
+map('n', "<leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<cr>", opts("Debugger run rust tests"))
+map('n', "<leader>dx", "<cmd>lua require('dap').terminate()<cr><cmd>lua require('dapui').close()<cr>", opts("Debugger terminate"))
 
 -- NOTE: Git
 map('n', "<leader>gb", "<cmd>lua require('snacks').git.blame_line()<cr>", opts("Git blame Line"))
