0fec1123b53ff4329966faa0c7f730f73e0764fc
Me1k0r <runner-me1k0r@proton.me>
2025-03-06 11:48:48 +0100

    edit: options - folds
    Added some native folds options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -19,6 +19,21 @@ vim.opt.fillchars = { eob = ' ' }
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+-- INFO: Folds
+vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+vim.opt.foldcolumn = "0"
+-- vim.opt.foldmethod = "indent"
+vim.opt.foldmethod = "expr"
+if vim.fn.has("nvim-0.10") == 1 then
+  vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+else
+  vim.opt.foldmethod = "indent"
+end
+
 -- INFO: File search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
