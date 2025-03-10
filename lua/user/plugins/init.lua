7aaf0409d50874adb878b2cf9662d914c871eee5
Me1k0r <runner-me1k0r@proton.me>
2025-03-10 13:14:13 +0100

    edit: plugins - organized plugin imports

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,23 +1,34 @@
 return {
-  require("user.plugins.base16"),
+  -- INFO: UI
   require("user.plugins.bufferline"),
-  require("user.plugins.blink"),
   require("user.plugins.ccc"),
-  require("user.plugins.crates"),
-  require("user.plugins.dap"),
   require("user.plugins.illuminate"),
+  require("user.plugins.lualine"),
+  require("user.plugins.noice"),
+
+  -- INFO: QoL
+  require("user.plugins.snacks"),
+  require("user.plugins.todo-comments"),
+  require("user.plugins.whichkey"),
+
+  -- INFO: LSP / Linting / Completion / Language support
+  require("user.plugins.treesitter"),
+  require("user.plugins.blink"),
+  require("user.plugins.crates"),
   require("user.plugins.inlay-hint"),
   require("user.plugins.lspconfig"),
-  require("user.plugins.lualine"),
   require("user.plugins.markdown"),
   require("user.plugins.mason"),
   require("user.plugins.mini"),
-  require("user.plugins.noice"),
   require("user.plugins.rainbowcsv"),
   require("user.plugins.rustaceanvim"),
-  require("user.plugins.snacks"),
-  require("user.plugins.todo-comments"),
-  require("user.plugins.tokyonight"),
-  require("user.plugins.treesitter"),
-  require("user.plugins.whichkey"),
+
+  -- INFO: Debug
+  require("user.plugins.dap"),
+
+  -- INFO: Colorschemes
+  require("user.plugins.base16"),
+  -- require("user.plugins.gruvbox"),
+  -- require("user.plugins.rosepine"),
+  -- require("user.plugins.tokyonight"),
 }
