85ac3b449e61f27a864440b69c68a9b5b7bef366
Me1k0r <runner-me1k0r@proton.me>
2025-03-12 12:17:45 +0100

    test: options - neovim-term bg consistency

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -58,6 +58,19 @@ vim.cmd "colorscheme vim" -- WARN: overwritten by plugins color scheme
 --vim.cmd "set background=dark"
 vim.opt.termguicolors = true
 
+-- INFO: Keep consistency between neovim and term bg color
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() io.write("\027]111\027\\") end,
+})
+
 -- INFO: Miscellaneous
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
