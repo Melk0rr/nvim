be5363928b4e2f6c9e8457b36d3fe5f715259cf3
Me1k0r <runner-me1k0r@proton.me>
2025-03-20 22:12:55 +0100

    wip: init - function to change colorscheme
    TODO: handle different hyde theme cases

--- init.lua
+++ init.lua
@@ -9,20 +9,20 @@ require("user.mappings")
 
 -- HACK: handling theme based on hyde
 -- INFO: Function to read config file
-function read_config(file_path)
+local function read_config(file_path)
   local handle = io.open(file_path, 'r')
   if not handle then
     error("Cannot open hyde config file: " .. file_path)
   end
 
   local content = handle:read("*a")
-  handle.close()
+  handle.close(file_path)
 
   return content
 end
 
 -- INFO: Function to extract hyde theme
-function get_hyde_theme(content)
+local function get_hyde_theme(content)
   for line in content:gmatch("[^\n]") do
     local theme = line:match('hydeTheme="([^"]+)"')
     if theme then
@@ -32,5 +32,20 @@ function get_hyde_theme(content)
   return nil
 end
 
+-- INFO: Function to change colorscheme based on hyde theme
+local function set_colorscheme(file_path)
+  local content = read_config(file_path)
+  if content then
+    local theme = get_hyde_theme(content)
+    if theme then
+      -- TODO: handle different colorschemes
+    else
+      print("Failed to read hyde conf")
+    end
+
+  end
+end
+
 local hyde_conf = os.getenv("HOME") .. "/.config/hyde/hyde.conf"
+set_colorscheme(hyde_conf)
 
