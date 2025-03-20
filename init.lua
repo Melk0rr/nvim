e5a7b8113083950647517f2e8a541b89e1aab8c8
Me1k0r <runner-me1k0r@proton.me>
2025-03-20 22:03:17 +0100

    wip: init - function to read a config file
    WIP: adding some function to handle hyde theme and change colorscheme
    accordingly
    Will move in a dedicated file later

--- init.lua
+++ init.lua
@@ -6,3 +6,19 @@ require("user.options")
 require("user.lazy")
 require("user.autocmd")
 require("user.mappings")
+
+-- HACK: handling theme based on hyde
+-- INFO: Function to read config file
+function read_config(file_path)
+  local handle = io.open(file_path, 'r')
+  if not handle then
+    error("Cannot open hyde config file: " .. file_path)
+  end
+
+  local content = handle:read("*a")
+  handle.close()
+
+  return content
+end
+
+
