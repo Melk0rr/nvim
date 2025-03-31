9f99dd16a63ba43375c49930335edc4d63d3a2ba
Me1k0r <runner-me1k0r@proton.me>
2025-03-31 22:29:44 +0200

    edit: plugins - colorscheme

--- lua/user/plugins/colorschemes.lua
+++ lua/user/plugins/colorschemes.lua
@@ -1,94 +1,94 @@
 return {
-  -- INFO: Catppuccin
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
-  },
-
-  -- INFO: Darkrose
-  {
-    "water-sucks/darkrose.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- INFO: Everblush
-  {
-    "Everblush/everblush.nvim",
-    priority = 1000,
-    name = 'everblush'
-  },
-
-  -- INFO: Evergarden
-  {
-    "comfysage/evergarden",
-    priority = 1000,
-    opts = {
-      theme = {
-        variant = 'fall', -- 'winter'|'fall'|'spring'
-        accent = 'green',
-      },
-    }
-  },
-
+  -- -- INFO: Catppuccin
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000
+  -- },
+  --
+  -- -- INFO: Darkrose
+  -- {
+  --   "water-sucks/darkrose.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Everblush
+  -- {
+  --   "Everblush/everblush.nvim",
+  --   priority = 1000,
+  --   name = 'everblush'
+  -- },
+  --
+  -- -- INFO: Evergarden
+  -- {
+  --   "comfysage/evergarden",
+  --   priority = 1000,
+  --   opts = {
+  --     theme = {
+  --       variant = 'fall', -- 'winter'|'fall'|'spring'
+  --       accent = 'green',
+  --     },
+  --   }
+  -- },
+  --
   -- INFO: Gruvbox
   {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = true,
   },
-
-  -- INFO: Iceberg
-  {
-    "oahlen/iceberg.nvim",
-    priority = 1000,
-  },
-
-  -- INFO: Kanagawa
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-  },
-
-  -- INFO: Lackluster (Mono)
-  {
-    "slugbyte/lackluster.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- INFO: Lavender
-  {
-    url = "https://codeberg.org/jthvai/lavender.nvim",
-    branch = "stable",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- INFO: Melange
-  {
-    "savq/melange-nvim",
-    priority = 1000,
-  },
-
-  -- INFO: Nord
-  {
-    "shaunsingh/nord.nvim",
-    name = "nord",
-    priority = 1000,
-  },
-
-  -- INFO: Rose pine
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-  },
-
-  -- INFO: Tokyonight
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-  },
+  --
+  -- -- INFO: Iceberg
+  -- {
+  --   "oahlen/iceberg.nvim",
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Kanagawa
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Lackluster (Mono)
+  -- {
+  --   "slugbyte/lackluster.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Lavender
+  -- {
+  --   url = "https://codeberg.org/jthvai/lavender.nvim",
+  --   branch = "stable",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Melange
+  -- {
+  --   "savq/melange-nvim",
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Nord
+  -- {
+  --   "shaunsingh/nord.nvim",
+  --   name = "nord",
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Rose pine
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   priority = 1000,
+  -- },
+  --
+  -- -- INFO: Tokyonight
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000,
+  -- },
 }
