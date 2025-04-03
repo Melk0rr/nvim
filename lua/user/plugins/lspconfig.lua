bb6a859ddf8d9071096e89f4606416533285f865
Me1k0r <runner-me1k0r@proton.me>
2025-04-03 10:24:26 +0200

    edit: plugins - lspconfig diagnostic signs
    Changed to not deprecated sign declaration method

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -159,11 +159,16 @@ return {
     })
 
     -- HACK: Change diagnostics symbols
-    local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
+    vim.diagnostic.config = {
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "󰅚 ",
+          [vim.diagnostic.severity.WARN] = "󰀪 ",
+          [vim.diagnostic.severity.HINT] = "󰌶 ",
+          [vim.diagnostic.severity.INFO] = " ",
+        }
+      }
+    }
 
     vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
       pattern = "*.wgsl",
