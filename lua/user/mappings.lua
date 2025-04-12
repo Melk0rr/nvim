2d73fc9f47108369ad85a4a158aba8cc062c7cff
Me1k0r <runner-me1k0r@proton.me>
2025-04-12 15:33:23 +0200

    edit: mappings - quote word

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -14,9 +14,10 @@ map({ 'n', 'v' }, "<C-c>", '"+y', opts("Yank to clipboard"))
 
 -- NOTE: Text manipulation
 map('n', "gA", "ggVG", { desc = "Select all" })
-map('n', 'gL', "i<cr><esc>", opts("Insert line break"))
-map('n', '<leader>p', "ma$p`a", opts("Prints at the end of the line"))
+map('n', "gL", "i<cr><esc>", opts("Insert line break"))
+map('n', "<leader>p", "ma$p`a", opts("Prints at the end of the line"))
 map('n', "<leader>;", "mqA;<esc>`q", opts("Semicolon at end of line"))
+map('n', 'gs"', 'gsaiw"', opts("Quotes word under cursor"))
 
 -- NOTE: Press jk fast to exit insert mode
 map('i', "jk", "<ESC>", opts("jk to exit insert mode"))
