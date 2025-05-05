ea6728bb4ce1d2e221fe41a8766c2bf242e915b1
me1k0r <runner-me1k0r@proton.me>
2025-05-05 14:23:37 +0200

    edit: mappings - quickfix + yazi mappings

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -70,7 +70,8 @@ map('n', "<leader>dk", "<cmd>lua require('dap').step_out()<cr>", opts("Debugger 
 map('n', "<leader>dl", "<cmd>lua require('dap').step_into()<cr>", opts("Debugger step into"))
 map({ 'n', 't' }, "<leader>dm", "<cmd>lua require('snacks').terminal.open({'make'}, { win = { position = 'bottom' } })<cr>", opts("Debugger run C make"))
 map('n', "<leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<cr>", opts("Debugger run rust tests"))
-map('n', "<leader>dx", "<cmd>lua require('dap').terminate()<cr><cmd>lua require('dapui').close()<cr>", opts("Debugger terminate"))
+map('n', "<leader>dx", "<cmd>lua require('dap').terminate()<cr><cmd>lua require('dapui').close()<cr>",
+  opts("Debugger terminate"))
 
 -- NOTE: Git
 map('n', "<leader>gb", "<cmd>lua require('snacks').git.blame_line()<cr>", opts("Git blame Line"))
@@ -112,26 +113,34 @@ map({ 'n', 't' }, "<leader>ntl", function () toggle_snacks_term("right") end, op
 map('n', "<leader>fa", "<cmd>lua require('snacks').picker.autocmds({ layout = 'vertical' })<cr>", opts("Find autocmds"))
 map('n', "<leader>fb", "<cmd>lua require('snacks').picker.buffers({ focus = 'list' })<cr>", opts("Find buffers"))
 map('n', "<leader>fB", "<cmd>lua require('snacks').picker.grep_buffers()<cr>", opts("Grep open buffers"))
-map('n', "<leader>fc", "<cmd>lua require('snacks').picker.files({ cwd = vim.fn.stdpath('config') })<cr>", opts("Find config files"))
+map('n', "<leader>fc", "<cmd>lua require('snacks').picker.files({ cwd = vim.fn.stdpath('config') })<cr>",
+  opts("Find config files"))
 map('n', "<leader>fC", "<cmd>lua require('snacks').picker.commands({ layout = 'select' })<cr>", opts("Find commands"))
 map('n', "<leader>ff", "<cmd>lua require('snacks').picker.files({ focus = 'input' })<cr>", opts("Find files"))
-map('n', "<leader>fg", "<cmd>lua require('snacks').picker.grep_word({ focus = 'list', layout = 'vertical' })<cr>", opts("Grep selection"))
+map('n', "<leader>fg", "<cmd>lua require('snacks').picker.grep_word({ focus = 'list', layout = 'vertical' })<cr>",
+  opts("Grep selection"))
 map('n', "<leader>fG", "<cmd>lua require('snacks').picker.grep()<cr>", opts("Live grep"))
-map('n', "<leader>fh", "<cmd>lua require('snacks').picker.highlights({ layout = 'dropdown', focus = 'list' })<cr>", opts("Find highlights"))
+map('n', "<leader>fh", "<cmd>lua require('snacks').picker.highlights({ layout = 'dropdown', focus = 'list' })<cr>",
+  opts("Find highlights"))
 map('n', "<leader>fH", "<cmd>lua require('snacks').picker.help({ layout = 'vertical' })<cr>", opts("Find help pages"))
 map('n', "<leader>fi", "<cmd>lua require('snacks').picker.icons()<cr>", opts("Find icons"))
 map('n', "<leader>fk", "<cmd>lua require('snacks').picker.keymaps({ layout = 'vertical' })<cr>", opts("Find keymaps"))
 map('n', "<leader>fm", "<cmd>lua require('snacks').picker.marks({ layout = 'vertical' })<cr>", opts("Find marks"))
 map('n', "<leader>fM", "<cmd>lua require('snacks').picker.man()<cr>", opts("Find man pages"))
-map('n', "<leader>fp", "<cmd>lua require('snacks').picker.projects({ layout = 'select', focus = 'list' })<cr>", opts("Find projects"))
+map('n', "<leader>fp", "<cmd>lua require('snacks').picker.projects({ layout = 'select', focus = 'list' })<cr>",
+  opts("Find projects"))
 map('n', "<leader>fr", "<cmd>lua require('snacks').picker.recent({ focus = 'list' })<cr>", opts("Find recent files"))
-map('n', "<leader>fS", "<cmd>lua require('snacks').picker.colorschemes({ layout = 'select', focus = 'input' })<cr>", opts("Find colorschemes"))
+map('n', "<leader>fS", "<cmd>lua require('snacks').picker.colorschemes({ layout = 'select', focus = 'input' })<cr>",
+  opts("Find colorschemes"))
 map('n', "<leader>ft", "<cmd>lua require('snacks').picker.todo_comments()<cr>", opts("Find Todo comments"))
-map('n', "<leader>fT", "<cmd>lua require('snacks').picker.todo_comments({ keywords = { 'TODO', 'FIX', 'FIXME' } })<cr>", opts("Find Todo/Fix/Fixme"))
+map('n', "<leader>fT", "<cmd>lua require('snacks').picker.todo_comments({ keywords = { 'TODO', 'FIX', 'FIXME' } })<cr>",
+  opts("Find Todo/Fix/Fixme"))
 
 -- NOTE: Picker search
-map('n', '<leader>s"', "<cmd>lua require('snacks').picker.registers({ layout = 'dropdown', focus = 'list' })<cr>", opts("Search registers"))
-map('n', "<leader>s:", "<cmd>lua require('snacks').picker.command_history({ focus = 'list' })<cr>", opts("Search command history"))
+map('n', '<leader>s"', "<cmd>lua require('snacks').picker.registers({ layout = 'dropdown', focus = 'list' })<cr>",
+  opts("Search registers"))
+map('n', "<leader>s:", "<cmd>lua require('snacks').picker.command_history({ focus = 'list' })<cr>",
+  opts("Search command history"))
 map('n', "<leader>s/", "<cmd>lua require('snacks').picker.search_history({ focus = 'list' })<cr>", opts("Search history"))
 map('n', "<leader>sh", "<cmd>noh<cr>", opts("No search highlight"))
 map('n', "<leader>sn", "<cmd>lua require('snacks').notifier.show_history()<cr>", opts("Search history"))
@@ -144,8 +153,10 @@ map('n', "<leader>lD", "<cmd>lua require('snacks').picker.diagnostics_buffer()<c
 map('n', "<leader>ll", "<cmd>lua require('snacks').words.jump(vim.v.count1)<cr>", opts("Next LSP reference"))
 map('n', "<leader>lL", "<cmd>lua require('snacks').words.jump(-vim.v.count1)<cr>", opts("Previous LSP reference"))
 map('n', "<leader>lR", "<cmd>lua require('snacks').picker.references()<cr>", opts("Find LSP references"))
-map('n', "<leader>ls", "<cmd>lua require('snacks').picker.lsp_symbols({ layout = 'vertical' })<cr>", opts("Find LSP symbols"))
-map('n', "<leader>lS", "<cmd>lua require('snacks').picker.lsp_workspace_symbols({ layout = 'vertical' })<cr>", opts("Find LSP workspace symbols"))
+map('n', "<leader>ls", "<cmd>lua require('snacks').picker.lsp_symbols({ layout = 'vertical' })<cr>",
+  opts("Find LSP symbols"))
+map('n', "<leader>lS", "<cmd>lua require('snacks').picker.lsp_workspace_symbols({ layout = 'vertical' })<cr>",
+  opts("Find LSP workspace symbols"))
 map('n', "<leader>lcp", "<cmd>CccPick<cr>", opts("Toggles color picker"))
 map('n', "<leader>lCd", "<cmd>RainbowDelim<cr>", opts("Rainbow CSV delim with character under cursor"))
 map('n', "<leader>lCa", "<cmd>RainbowAlign<cr>", opts("Rainbow CSV align"))
@@ -190,7 +201,7 @@ local function fold_markdown_headings(levels)
 end
 
 -- HACK: Use <CR> to fold in normal mode
-map('n', "<CR>", function ()
+map('n', "<CR>", function()
   -- Get current line number
   local line = vim.fn.line(".")
 
