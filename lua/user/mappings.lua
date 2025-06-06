fc855d1e9513659e89d6599c9285902e113982e2
Me1k0r <runner-me1k0r@proton.me>
2025-06-06 12:57:23 +0200

    test: mappings - more chars + di

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -18,9 +18,10 @@ map('n', "gL", "i<cr><esc>", opts("Insert line break"))
 map('n', "<leader>p", "ma$p`a", opts("Prints at the end of the line"))
 map('n', "<leader>;", "mqA;<esc>`q", opts("Semicolon at end of line"))
 
-for _, char in ipairs({ '/', '|', ':' }) do
+for _, char in ipairs({ '/', '\\', '|', ':', ';', ',', '.' }) do
   map('n', "vi" ..char, "<cmd>normal! T" ..char.. "vt" ..char.. "<cr>", opts("inner " ..char))
   map('n', "ci" ..char, "<cmd>normal! T" ..char.. "ct" ..char.. "<cr>li", opts("inner " ..char))
+  map('n', "di" ..char, "<cmd>normal! T" ..char.. "dt" ..char.. "<cr>l", opts("inner " ..char))
 end
 
 -- NOTE: Press jk fast to exit insert mode
