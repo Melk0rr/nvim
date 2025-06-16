2087ebedc4621bc95cd5e934f505eac8ce6df183
me1k0r <runner-me1k0r@proton.me>
2025-06-16 09:51:33 +0200

    edit: mappings - yi for custom chars

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -23,6 +23,7 @@ for _, char in ipairs({ '/', '\\', '|', ':', ';', ',', '.' }) do
   map('n', "vi" ..char, "<cmd>normal! T" ..char.. "vt" ..char.. "<cr>", opts("inner " ..char))
   map('n', "ci" ..char, "<cmd>normal! T" ..char.. "ct" ..char.. "<cr>li", opts("inner " ..char))
   map('n', "di" ..char, "<cmd>normal! T" ..char.. "dt" ..char.. "<cr>", opts("inner " ..char))
+  map('n', "yi" ..char, "<cmd>normal! T" ..char.. "yt" ..char.. "<cr>", opts("inner " ..char))
 end
 
 -- NOTE: Press jk fast to exit insert mode
