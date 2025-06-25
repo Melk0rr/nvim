982a34946976a6f797c440229ef4a8c3ad77bcf4
Me1k0r <runner-me1k0r@proton.me>
2025-06-25 22:29:55 +0200

    test: plugins/blink - trying to figure out why json snippets are not loaded 2

--- lua/user/plugins/blink.lua
+++ lua/user/plugins/blink.lua
@@ -27,31 +27,7 @@ return {
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
       providers = {
-        lsp = {
-          name = "lsp",
-          enabled = true,
-          module = "blink.cmp.sources.lsp",
-          kind = "LSP",
-          score_offset = 90, -- the higher the number, the higher the priority
-        },
-        path = {
-          name = "Path",
-          module = "blink.cmp.sources.path",
-          score_offset = 25,
-          fallbacks = { "snippets", "buffer" },
-        },
-        buffer = {
-          name = "Buffer",
-          enabled = true,
-          max_items = 3,
-          module = "blink.cmp.sources.buffer",
-          score_offset = 15, -- the higher the number, the higher the priority
-        },
         snippets = {
-          name = "snippets",
-          enabled = true,
-          module = "blink.cmp.sources.snippets",
-          score_offset = 85, -- the higher the number, the higher the priority
           opts = {
             friendly_snippets = true,
             search_paths = { vim.fn.stdpath('config') .. '/snippets' },
@@ -59,7 +35,6 @@ return {
         },
       }
     },
-    snippets = { preset = "luasnip" },
     signature = { enabled = true }
   },
 }
