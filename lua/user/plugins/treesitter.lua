1f082c96dda235f98f2598545e5a396e6f02ea2a
Me1k0r <runner-me1k0r@proton.me>
2025-06-25 19:28:11 +0200

    edit: plugins/treesitter - context plugin dependency

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "nvim-treesitter-context" },
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
