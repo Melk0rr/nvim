191f208c72e0139d4cb7067db6e118b24207362a
Me1k0r <runner-me1k0r@proton.me>
2025-06-25 19:31:23 +0200

    fix: plugins/blink - ghost_text out of place

--- lua/user/plugins/blink.lua
+++ lua/user/plugins/blink.lua
@@ -18,8 +18,8 @@ return {
         selection = { preselect = false },
       },
       menu = { auto_show = true },
+      ghost_text = { enabled = true },
     },
-    ghost_text = { enabled = true },
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
