2cdc86d83ae45370800635f67a1ded833d2326be
Me1k0r <runner-me1k0r@proton.me>
2025-06-26 11:15:16 +0200

    edit: plugins/luasnip - lazy_load for friendly snippets

--- lua/user/plugins/luasnip.lua
+++ lua/user/plugins/luasnip.lua
@@ -7,9 +7,10 @@ return {
   },
   config = function ()
     local ls = require("luasnip")
-    ls.setup({ enable_autosnippets = true })
+    ls.config.setup({ enable_autosnippets = true })
 
     -- Custom snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
     require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath('config') .. '/snippets' } })
 
     -- Documentation snippets
