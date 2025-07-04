ea54de2ad5296872793441bee003343cfc7c6744
Me1k0r <runner-me1k0r@proton.me>
2025-07-04 09:21:02 +0200

    edit: plugins/dap - c and cpp config + format

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -11,41 +11,36 @@ return {
 
       require("dap-python").setup()
 
-      dap.listeners.before.attach.dapui_config = function ()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function ()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function ()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function ()
-        dapui.close()
-      end
-
       dap.adapters.gdb = {
         type = "executable",
         command = "gdb",
-        args = { "--interpreter=dap", "--eval-command", "set print pretty on"},
+        args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
       }
-      dap.configurations.c = {
-        {
-          name = "Launch",
-          type = "gdb",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
-        },
+
+      local c_cpp_config = {
+        name = "Launch",
+        type = "gdb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
       }
+
+      dap.configurations.c = c_cpp_config
+      dap.configurations.cpp = c_cpp_config
+
+      dap.listeners.before.attach.dapui_config = function() dapui.open() end
+      dap.listeners.before.launch.dapui_config = function() dapui.open() end
+      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
     end,
   },
 
   {
     "rcarriga/nvim-dap-ui",
     dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-    config = function ()
+    config = function()
       require("dapui").setup()
     end
   }
