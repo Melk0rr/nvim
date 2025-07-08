e38057d655ffa950b39d81ea3b3255a134afae4a
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 21:48:05 +0200

    edit: plugins/blink - keymap preset

--- lua/user/plugins/blink.lua
+++ lua/user/plugins/blink.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   opts = {
     keymap = {
+      preset = "default",
       -- ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
       -- ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       ["<Tab>"]    = { "snippet_forward", "fallback_to_mappings" },
