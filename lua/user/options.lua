f5b04762d436ee7295bb749f5b82880d5e3fb70d
Me1k0r <runner-me1k0r@proton.me>
2025-07-09 15:39:36 +0200

    edit: options - categories + tweaked some stuff

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,11 @@
--- INFO: Tabulation
+-- ===========================================================================
+-- NOTE: Tabulation
+-- ===========================================================================
 vim.opt.expandtab = true -- tab to space
 
+-- ===========================================================================
 -- WARN: Can be overwritten by lsp
+-- ===========================================================================
 vim.opt.tabstop = 2 -- tab size
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
@@ -10,17 +14,26 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.smartcase = true
 
--- INFO: Line
+-- ===========================================================================
+-- NOTE: Line
+-- ===========================================================================
 vim.opt.cursorline = true
 vim.opt.number = true -- HACK: line number
 vim.opt.fillchars = { eob = ' ' }
 
--- INFO: Split
+-- ===========================================================================
+-- NOTE: Window splitting and buffers
+-- ===========================================================================
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.o.hidden = true
 
--- INFO: Folds
-vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.switchbuf = "useopen"
+
+-- ===========================================================================
+-- NOTE: Folds
+-- ===========================================================================
+vim.opt.foldlevel = 999 -- Using ufo provider need a large value, feel free to decrease the value
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 vim.opt.foldcolumn = "0"
@@ -37,28 +50,47 @@ else
   vim.opt.foldmethod = "indent"
 end
 
--- INFO: File search
+-- ===========================================================================
+-- NOTE: Trailing whitespace
+-- ===========================================================================
+
+vim.o.list = true
+vim.opt.listchars = {
+  extends = "»",
+  nbsp = "⣿",
+  precedes = "«",
+  trail = "·",
+  -- removed tab, it's too noisy
+  tab = "  ", -- this must be two chars, see :h listchars
+}
+
+-- ===========================================================================
+-- NOTE: File search
+-- ===========================================================================
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
--- INFO: Status
+-- ===========================================================================
+-- NOTE: Status
+-- ===========================================================================
 vim.opt.laststatus = 2
 vim.opt.showcmd = true
 vim.opt.showmode = false
 
 vim.opt.ch = 0
 
--- INFO: Term
+-- ===========================================================================
+-- NOTE: Term
+-- ===========================================================================
 vim.g.terminal_emulator = "ghostty"
 
--- INFO: Term colors
 vim.cmd "colorscheme vim" -- WARN: overwritten by plugins color scheme
 --vim.cmd "set background=dark"
 vim.opt.termguicolors = true
 
--- INFO: Keep consistency between neovim and term bg color
+-- HACK: Keep consistency between neovim and term bg color
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
   callback = function()
     local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
@@ -71,14 +103,18 @@ vim.api.nvim_create_autocmd("UILeave", {
   callback = function() io.write("\027]111\027\\") end,
 })
 
--- INFO: Miscellaneous
+-- ===========================================================================
+-- NOTE: Miscellaneous
+-- ===========================================================================
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.pumheight = 40
 vim.lsp.inlay_hint.enable(true)
 
 
+-- ===========================================================================
 -- HACK: Change diagnostics symbols
+-- ===========================================================================
 vim.diagnostic.config {
   virtual_text = true,
   signs = {
