c918b9e8ba315298623684a205ecb857f617577e
Me1k0r <runner-me1k0r@proton.me>
2025-07-15 18:56:58 +0200

    fix: plugins/heirline - proper way to retrieve diagnostic signs

--- lua/user/plugins/heirline/common.lua
+++ lua/user/plugins/heirline/common.lua
@@ -1,41 +1,40 @@
 local icons = {
-    -- ✗   󰅖 󰅘 󰅚 󰅙 󱎘 
-    close = "󰅙 ",
-    dir = "󰉋 ",
-    lsp = " ", --   
-    vim = " ",--       
-    debug = " ",
-    rec = " ",
-    modified = "● ",
-    readonly = " ",
-    terminal = "  ",
-    err = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
-    warn = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
-    info = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
-    hint = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+  -- ✗   󰅖 󰅘 󰅚 󰅙 󱎘 
+  close = "󰅙 ",
+  dir = "󰉋 ",
+  lsp = " ", --   
+  vim = " ", --       
+  debug = " ",
+  rec = " ",
+  modified = "● ",
+  readonly = " ",
+  terminal = "  ",
+  err = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.ERROR],
+  war = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.WARN],
+  info = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.INFO],
+  hint = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.HINT],
 }
 
 local separators = {
-    rounded_left = "",
-    rounded_right = "",
-    rounded_left_hollow = "",
-    rounded_right_hollow = "",
-    powerline_left = "",
-    powerline_right = "",
-    powerline_right_hollow = "",
-    powerline_left_hollow = "",
-    slant_left = "",
-    slant_right = "",
-    inverted_slant_left = " ",
-    inverted_slant_right = "",
-    slant_ur = "",
-    slant_br = "",
-    vert = "│",
-    vert_thick = "┃",
-    block = "█",
-    double_vert = "║",
-    dotted_vert = "┊",
+  rounded_left = "",
+  rounded_right = "",
+  rounded_left_hollow = "",
+  rounded_right_hollow = "",
+  powerline_left = "",
+  powerline_right = "",
+  powerline_right_hollow = "",
+  powerline_left_hollow = "",
+  slant_left = "",
+  slant_right = "",
+  inverted_slant_left = " ",
+  inverted_slant_right = "",
+  slant_ur = "",
+  slant_br = "",
+  vert = "│",
+  vert_thick = "┃",
+  block = "█",
+  double_vert = "║",
+  dotted_vert = "┊",
 }
 
 return { separators = separators, icons = icons }
-
