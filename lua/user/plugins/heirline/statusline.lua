f547485144afb84144f6d93b44e41fb93ae72901
Me1k0r <runner-me1k0r@proton.me>
2025-07-15 22:34:02 +0200

    edit: plugins/heirline - sections + file icon component

--- lua/user/plugins/heirline/statusline.lua
+++ lua/user/plugins/heirline/statusline.lua
@@ -4,6 +4,9 @@ local utils = require("heirline.utils")
 local separators = require("user.plugins.heirline.common").separators
 local icons = require("user.plugins.heirline.common").icons
 
+-- ===========================================================================
+-- INFO: Mode
+-- ===========================================================================
 local Mode = {
   init = function(self)
     self.mode = vim.fn.mode(1)
@@ -62,73 +65,93 @@ local Mode = {
   },
 }
 
-Mode = utils.surround({ separators.rounded_left, separators.rounded_right }, "bright_bg", {Mode})
-
-local DefaultStatusline = {
-    Mode,
-    -- Space,
-    -- Spell,
-    -- WorkDir,
-    -- FileNameBlock,
-    -- { provider = "%<" },
-    -- Space,
-    -- Git,
-    -- Space,
-    -- Diagnostics,
-    -- Align,
-    -- -- { flexible = 3,   { Navic, Space }, { provider = "" } },
-    -- Align,
-    -- DAPMessages,
-    -- LSPActive,
-    -- -- VirtualEnv,
-    -- Space,
-    -- FileType,
-    -- { flexible = 3,   { FileEncoding, Space }, { provider = "" } },
-    -- Space,
-    -- Ruler,
-    -- SearchCount,
-    -- Space,
-    -- ScrollBar,
+-- ===========================================================================
+-- INFO: File
+-- ===========================================================================
+local FileIcon = {
+  init = function(self)
+    local filename = self.filename
+    local extension = vim.fn.fnamemodify(filename, ":e")
+    self.icon, self.icon_color =
+        require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+  end,
+  provider = function(self)
+    return self.icon and (self.icon .. " ")
+  end,
+  hl = function(self)
+    return { fg = self.icon_color }
+  end,
 }
 
+Mode = utils.surround({ separators.rounded_left, separators.rounded_right }, "bright_bg", { Mode })
+
+local DefaultStatusline = {
+  Mode,
+  -- Space,
+  -- Spell,
+  -- WorkDir,
+  -- FileNameBlock,
+  -- { provider = "%<" },
+  -- Space,
+  -- Git,
+  -- Space,
+  -- Diagnostics,
+  -- Align,
+  -- -- { flexible = 3,   { Navic, Space }, { provider = "" } },
+  -- Align,
+  -- DAPMessages,
+  -- LSPActive,
+  -- -- VirtualEnv,
+  -- Space,
+  -- FileType,
+  -- { flexible = 3,   { FileEncoding, Space }, { provider = "" } },
+  -- Space,
+  -- Ruler,
+  -- SearchCount,
+  -- Space,
+  -- ScrollBar,
+}
+
+-- ===========================================================================
+-- INFO: Final status lines
+-- ===========================================================================
 local StatusLines = {
   hl = function()
-        if conditions.is_active() then
-            return "StatusLine"
-        else
-            return "StatusLineNC"
-        end
-    end,
-    static = {
-        mode_colors = {
-            n = "cyan",
-            i = "green",
-            v = "blue",
-            V = "blue",
-            ["\22"] = "blue", -- this is an actual ^V, type <C-v><C-v> in insert mode
-            c = "orange",
-            s = "pure",
-            S = "purple",
-            ["\19"] = "purple", -- this is an actual ^S, type <C-v><C-s> in insert mode
-            R = "orange",
-            r = "orange",
-            ["!"] = "red",
-            t = "green",
-        },
-        mode_color = function(self)
-            local mode = conditions.is_active() and vim.fn.mode() or "n"
-            return self.mode_colors[mode]
-        end,
+    if conditions.is_active() then
+      return "StatusLine"
+    else
+      return "StatusLineNC"
+    end
+  end,
+  static = {
+    mode_colors = {
+      n = "cyan",
+      i = "green",
+      v = "blue",
+      V = "blue",
+      ["\22"] = "blue", -- this is an actual ^V, type <C-v><C-v> in insert mode
+      c = "orange",
+      s = "pure",
+      S = "purple",
+      ["\19"] = "purple", -- this is an actual ^S, type <C-v><C-s> in insert mode
+      R = "orange",
+      r = "orange",
+      ["!"] = "red",
+      t = "green",
     },
-    fallthrough = false,
-    -- GitStatusline,
-    -- SpecialStatusline,
-    -- TerminalStatusline,
-    -- InactiveStatusline,
-    DefaultStatusline,
+    mode_color = function(self)
+      local mode = conditions.is_active() and vim.fn.mode() or "n"
+      return self.mode_colors[mode]
+    end,
+  },
+  fallthrough = false,
+  -- GitStatusline,
+  -- SpecialStatusline,
+  -- TerminalStatusline,
+  -- InactiveStatusline,
+  DefaultStatusline,
 }
 
 return {
   statusline = StatusLines
 }
-
