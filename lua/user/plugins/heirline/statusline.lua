99806b4ac4f4336ac4f3b86720d20d903d00f99e
Me1k0r <runner-me1k0r@proton.me>
2025-07-16 13:15:43 +0200

    edit: plugins/heirline - filename and filename block

--- lua/user/plugins/heirline/statusline.lua
+++ lua/user/plugins/heirline/statusline.lua
@@ -83,6 +83,47 @@ local FileIcon = {
   end,
 }
 
+local FileName = {
+  init = function(self)
+    self.lfilename = vim.fn.fnamemodify(self.filename, ":.")
+    if self.lfilename == "" then
+      self.lfilename = "[No Name]"
+    end
+    if not conditions.width_percent_below(#self.lfilename, 0.27) then
+      self.lfilename = vim.fn.pathshorten(self.lfilename)
+    end
+  end,
+  hl = function()
+    if vim.bo.modified then
+      return { fg = utils.get_highlight("Directory").fg, bold = true, italic = true }
+    end
+    return "Directory"
+  end,
+  flexible = 2,
+  {
+    provider = function(self)
+      return self.lfilename
+    end,
+  },
+  {
+    provider = function(self)
+      return vim.fn.pathshorten(self.lfilename)
+    end,
+  },
+}
+
+local FileNameBlock = {
+    init = function(self)
+        self.filename = vim.api.nvim_buf_get_name(0)
+    end,
+    FileIcon,
+    FileName,
+    -- unpack(FileFlags),
+}
+
+-- ===========================================================================
+-- INFO: Wrap up default status line
+-- ===========================================================================
 Mode = utils.surround({ separators.rounded_left, separators.rounded_right }, "bright_bg", { Mode })
 
 local DefaultStatusline = {
@@ -90,7 +131,7 @@ local DefaultStatusline = {
   -- Space,
   -- Spell,
   -- WorkDir,
-  -- FileNameBlock,
+  FileNameBlock,
   -- { provider = "%<" },
   -- Space,
   -- Git,
