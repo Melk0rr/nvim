fb596650e3e016a1eab635d229bbf94fdf3cabca
Me1k0r <runner-me1k0r@proton.me>
2025-07-16 18:33:35 +0200

    edit: plugins/heirline - git component

--- lua/user/plugins/heirline/statusline.lua
+++ lua/user/plugins/heirline/statusline.lua
@@ -187,6 +187,65 @@ local WorkDir = {
 }
 
 -- ===========================================================================
+-- INFO: Git
+-- ===========================================================================
+local Git = {
+  condition = conditions.is_git_repo,
+  init = function(self)
+    self.status_dict = vim.b.gitsigns_status_dict
+    self.has_changes = self.status_dict.added ~= 0 or self.status_dict.removed ~= 0 or self.status_dict.changed ~= 0
+  end,
+  on_click = {
+    callback = function(self, minwid, nclicks, button)
+      vim.defer_fn(function()
+        vim.cmd("Lazygit %:p:h")
+      end, 100)
+    end,
+    name = "heirline_git",
+    update = false,
+  },
+  hl = { fg = "orange" },
+  {
+    provider = function(self)
+      return " " .. self.status_dict.head
+    end,
+    hl = { bold = true },
+  },
+  {
+    condition = function(self)
+      return self.has_changes
+    end,
+    provider = "(",
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.added or 0
+      return count > 0 and ("+" .. count)
+    end,
+    hl = "diffAdded",
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.removed or 0
+      return count > 0 and ("-" .. count)
+    end,
+    hl = "diffDeleted",
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.changed or 0
+      return count > 0 and ("~" .. count)
+    end,
+    hl = "diffChanged",
+  },
+  {
+    condition = function(self)
+      return self.has_changes
+    end,
+    provider = ")",
+  },
+}
+-- ===========================================================================
 -- INFO: Wrap up default status line
 -- ===========================================================================
 
