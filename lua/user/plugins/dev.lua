a6a83ed6d506d58ab2471c6ce465303c6ae112c1
Me1k0r <runner-me1k0r@proton.me>
2025-07-17 12:31:03 +0200

    edit: plugins/dev - centralized dev tools

---
+++ lua/user/plugins/dev.lua
@@ -0,0 +1,103 @@
+-- INFO: Multiple plugins for dev QoL
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    opts = {
+      trouble = true,
+      preview_config = {},
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = { "Trouble" },
+    keys = "<leader>t",
+    opts = {
+      -- preview = { type = "float" },
+      open_no_results = true,
+      keys = {
+        ["<Tab>"] = "fold_toggle",
+      },
+      icons = {
+        kinds = require("lsp.init").symbol_icons,
+      },
+      modes = {
+        symbols = {
+          win = {
+            size = 0.2,
+          },
+          filter = {
+            any = {
+              kind = {
+                "Variable",
+                "Class",
+                "Constructor",
+                "Enum",
+                "Field",
+                "Function",
+                "Interface",
+                "Method",
+                "Module",
+                "Namespace",
+                "Package",
+                "Property",
+                "Struct",
+                "Trait",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
+  {
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      backend      = "kitty",
+      processor    = "magick_cli",
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
+          filetypes = { "markdown", "vimwiki", "html" },
+        },
+        html = {
+          enabled = true,
+          only_render_image_at_cursor = true,
+          filetypes = { "html", "xhtml", "htm", "markdown" }
+        },
+        css = {
+          enabled = true
+        }
+      }
+    }
+  }
+}
