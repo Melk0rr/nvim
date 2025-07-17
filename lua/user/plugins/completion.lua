6cadddf88c651e05819281f97ab4d36099ec823e
Me1k0r <runner-me1k0r@proton.me>
2025-07-17 14:54:59 +0200

    edit: plugins/mini - merged with completion plugin

--- lua/user/plugins/completion.lua
+++ lua/user/plugins/completion.lua
@@ -55,5 +55,50 @@ return {
         highlight_group = "LspInlayHint",
       })
     end
-  }
+  },
+
+  -- NOTE: Mini - Autopair
+  {
+    "echasnovski/mini.pairs",
+    version = '*',
+    opts = {
+      modes = { insert = true, command = false, terminal = true },
+      mappings = {
+        ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+        ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+        ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+
+        [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+        [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+        ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+
+        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
+        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+      },
+    }
+  },
+
+  -- NOTE: Mini - Comment
+  {
+    "echasnovski/mini.comment",
+    version = '*',
+  },
+
+  -- NOTE: Mini - Surround
+  {
+    "echasnovski/mini.surround",
+    version = '*',
+    opts = {
+      mappings = {
+        add = "gsa",                -- Add surrounding in Normal and Visual modes
+        delete = "gsd",             -- Delete surrounding
+        find = "gsf",               -- Find surrounding (to the right)
+        find_left = "gsF",          -- Find surrounding (to the left)
+        highlight = "gsh",          -- Highlight surrounding
+        replace = "gsr",            -- Replace surrounding
+        update_n_lines = "gsn",     -- Update `n_lines`
+      }
+    }
+  },
 }
