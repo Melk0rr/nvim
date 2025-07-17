44ef884f64ff11816f34e52f4ef90f71da3665bd
Me1k0r <runner-me1k0r@proton.me>
2025-07-17 14:55:47 +0200

    edit: plugins/dev - organization

--- lua/user/plugins/dev.lua
+++ lua/user/plugins/dev.lua
@@ -10,6 +10,53 @@ return {
     },
   },
 
+  -- NOTE: Image
+  {
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      backend      = "kitty",
+      processor    = "magick_cli",
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
+          filetypes = { "markdown", "vimwiki", "html" },
+        },
+        html = {
+          enabled = true,
+          only_render_image_at_cursor = true,
+          filetypes = { "html", "xhtml", "htm", "markdown" }
+        },
+        css = {
+          enabled = true
+        }
+      }
+    }
+  },
+
+  -- NOTE: Rainbow CSV
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
+
   -- NOTE: Render Markdown
   {
     "MeanderingProgrammer/render-markdown.nvim",
@@ -72,51 +119,4 @@ return {
       },
     },
   },
-
-  -- NOTE: Rainbow CSV
-  {
-    "cameron-wags/rainbow_csv.nvim",
-    config = true,
-    ft = {
-      "csv",
-      "tsv",
-      "csv_semicolon",
-      "csv_whitespace",
-      "csv_pipe",
-      "rfc_csv",
-      "rfc_semicolon",
-    },
-    cmd = {
-      "RainbowDelim",
-      "RainbowDelimSimple",
-      "RainbowDelimQuoted",
-      "RainbowMultiDelim",
-    },
-  },
-
-  -- NOTE: Image
-  {
-    "3rd/image.nvim",
-    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-    opts = {
-      backend      = "kitty",
-      processor    = "magick_cli",
-      integrations = {
-        markdown = {
-          enabled = true,
-          clear_in_insert_mode = false,
-          only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
-          filetypes = { "markdown", "vimwiki", "html" },
-        },
-        html = {
-          enabled = true,
-          only_render_image_at_cursor = true,
-          filetypes = { "html", "xhtml", "htm", "markdown" }
-        },
-        css = {
-          enabled = true
-        }
-      }
-    }
-  }
 }
