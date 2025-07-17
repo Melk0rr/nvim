844d3bb94e6f35b84cab1986215b211a336d630c
Me1k0r <runner-me1k0r@proton.me>
2025-07-17 11:39:50 +0200

    rename: plugins/completion - centralized blink and inlay-hint into a completion related plugin

---
+++ lua/user/plugins/completion.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    dependencies = { "moyiz/blink-emoji.nvim" },
+    lazy = false,
+    opts = {
+      keymap = {
+        preset    = "default",
+        -- ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        -- ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<CR>"]  = { "accept", "fallback" },
+        ["<A-k>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<A-j>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+        ["<S-j>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-e>"] = { "hide", "fallback" },
+      },
+      completion = {
+        keyword = { range = "full" },
+        list = {
+          selection = { preselect = false },
+        },
+        menu = { auto_show = true, border = "rounded" },
+        ghost_text = { enabled = true },
+        documentation = { auto_show = true },
+      },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "emoji" },
+        providers = {
+          path = { fallbacks = { "snippets", "buffer" } },
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+          }
+        }
+      },
+      snippets = { preset = "luasnip" },
+      signature = { enabled = true },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" }
+  },
+  {
+    "felpafel/inlay-hint.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+    event = "LspAttach",
+    config = function()
+      require("inlay-hint").setup({
+        virt_text_pos = "inline",
+        highlight_group = "LspInlayHint",
+      })
+    end
+  }
+}
