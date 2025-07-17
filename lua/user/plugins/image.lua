8dcfed2c328747293ee505df55129c4eec6619d4
Me1k0r <runner-me1k0r@proton.me>
2025-07-17 11:30:27 +0200

    edit: plugins/image - format

--- lua/user/plugins/image.lua
+++ lua/user/plugins/image.lua
@@ -1,10 +1,10 @@
 return {
   "3rd/image.nvim",
-  build = false,   -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
   opts = {
-    backend = "kitty",
-    processor = "magick_cli",
-    integrations  = {
+    backend      = "kitty",
+    processor    = "magick_cli",
+    integrations = {
       markdown = {
         enabled = true,
         clear_in_insert_mode = false,
