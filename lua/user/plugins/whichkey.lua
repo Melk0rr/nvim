cc7080655dfcbaccccbc9d9eca15847e8747813e
Me1k0r <runner-me1k0r@proton.me>
2025-07-17 12:30:02 +0200

    edit: plugins/whichkey - trouble group + icon sizes

--- lua/user/plugins/whichkey.lua
+++ lua/user/plugins/whichkey.lua
@@ -6,7 +6,7 @@ return {
     wk.setup({ preset = "modern" })
     wk.add({
       -- { "<leader>" , group = "Leader" },
-      { "<leader>?" , function () require("which-key").show({ global = false }) end, desc = "Show local keymaps", icon = { icon = "󰋖", color = "yellow" } },
+      { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "Show local keymaps", icon = { icon = "󰋖", color = "yellow" } },
 
       -- NOTE: Buffer group
       { "<leader>b", group = "Buffer" },
@@ -14,38 +14,40 @@ return {
       { "<leader>bB", "<cmd>bnext<cr>", desc = "Previous Buffer" },
 
       -- NOTE: Debug group
-      { "<leader>d", group = "Debug", icon = { icon = "󰃤", color = "red" } },
+      { "<leader>d", group = "Debug", icon = { icon = "󰃤 ", color = "red" } },
+
+      -- NOTE: Trouble group
+      { "<leader>t", group = "Trouble", icon = { icon = " ", color = "red" } },
 
       -- NOTE: Fuzzy finder group
-      { "<leader>f", group = "Fuzzy Finder", icon = { icon = "󰭎", color = "blue" } },
+      { "<leader>f", group = "Fuzzy Finder", icon = { icon = "󰭎 ", color = "blue" } },
 
       -- NOTE: Git group
-      { "<leader>g", group = "Git", icon = { icon = "", color = "orange" } },
+      { "<leader>g", group = "Git", icon = { icon = " ", color = "orange" } },
 
       -- NOTE: LSP group
-      { "<leader>l", group = "LSP", icon = { icon = "", color = "green" } },
+      { "<leader>l", group = "LSP", icon = { icon = " ", color = "green" } },
 
       -- NOTE: Navigation
       { "<leader>n", group = "Navigation", icon = { icon = "󰎐", color = "yellow" } },
 
       -- NOTE: Explorer subgroup
-      { "<leader>ne", group = "Explorer", icon = { icon = "", color = "yellow" } },
+      { "<leader>ne", group = "Explorer", icon = { icon = " ", color = "yellow" } },
 
       -- NOTE: Terminal subgroup
-      { "<leader>nt", group = "Terminal", icon = { icon = "", color = "green" } },
+      { "<leader>nt", group = "Terminal", icon = { icon = " ", color = "green" } },
 
       -- NOTE: Command groups
-      { "<leader>:", group = "Commands", icon = { icon = "", color = "green" } },
-      { "<leader>:w", group = "Save", icon = { icon = "󰆓", color = "blue" } },
-      { "<leader>:q", group = "Quit", icon = { icon = "󰈆", color = "red" } },
+      { "<leader>:", group = "Commands", icon = { icon = " ", color = "green" } },
+      { "<leader>:w", group = "Save", icon = { icon = "󰆓 ", color = "blue" } },
+      { "<leader>:q", group = "Quit", icon = { icon = "󰈆 ", color = "red" } },
 
       -- NOTE: Search group
-      { "<leader>s", group = "Search", icon = { icon = "", color = "blue" } },
+      { "<leader>s", group = "Search", icon = { icon = " ", color = "blue" } },
 
       -- NOTE: Text insertion group
       { "<leader>i", group = "Insertion", icon = { icon = "󰗧", color = "blue" } },
     }
-  )
+    )
   end
 }
-
