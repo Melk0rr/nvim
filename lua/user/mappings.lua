443a325b68416b574eb5bc76276c6aec31c24526
Me1k0r <runner-me1k0r@proton.me>
2025-07-17 11:29:39 +0200

    edit: mappings - organization and tweaks

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -5,23 +5,29 @@ local opts = function(desc)
   return { noremap = true, silent = true, desc = desc }
 end
 
--- NOTE: Common
-map('n', "gA", "ggVG", { desc = "Select all" })
-
+-- ===========================================================================
 -- NOTE: Commands
+-- ===========================================================================
 map('n', "<leader>:qq", "<cmd>qa!<cr>", opts("Flip the table quit"))
 map('n', "<leader>:wqq", "<cmd>wqa!<cr>", opts("Save and quit"))
 
+-- ===========================================================================
 -- NOTE: Yank
+-- ===========================================================================
+map('n', "vA", "ggVG", { desc = "Select all" })
 map('n', 'Y', 'yy', opts("Yank whole line"))
 map({ 'n', 'v' }, "<C-c>", '"+y', opts("Yank to clipboard"))
 
+-- ===========================================================================
 -- NOTE: Text insertion
+-- ===========================================================================
 map('n', "<leader>iL", "i<cr><esc>", opts("Insert line break"))
 map('n', "<leader>ip", "ma$p`a", opts("Insert print at the end of the line"))
 map('n', "<leader>i;", "mqA;<esc>`q", opts("Insert semicolon at end of line"))
 
--- NOTE: Add vi, ci, and di keymaps for custom characters
+-- ===========================================================================
+-- NOTE: Custom motions
+-- ===========================================================================
 for _, char in ipairs({ '/', '\\', '|', ':', ';', ',', '.', ' ' }) do
   map('n', "vi" ..char, "<cmd>normal! T" ..char.. "vt" ..char.. "<cr>", opts("inner " ..char))
   map('n', "ci" ..char, "<cmd>normal! T" ..char.. "ct" ..char.. "<cr>li", opts("inner " ..char))
@@ -29,9 +35,61 @@ for _, char in ipairs({ '/', '\\', '|', ':', ';', ',', '.', ' ' }) do
   map('n', "yi" ..char, "<cmd>normal! T" ..char.. "yt" ..char.. "<cr>", opts("inner " ..char))
 end
 
+-- ===========================================================================
 -- NOTE: Press jk fast to exit insert mode
+-- ===========================================================================
 map('i', "jk", "<ESC>", opts("jk to exit insert mode"))
 map('i', "kj", "<ESC>", opts("kj to exit insert mode"))
+
+
+-- ===========================================================================
+-- NOTE: Buffer navigation
+-- ===========================================================================
+map('n', "<Tab>", "<cmd>bnext<cr>", opts("Next buffer"))
+map('n', "<S-Tab>", "<cmd>bprevious<cr>", opts("Previous buffer"))
+map('n', "<leader>bd", "<cmd>lua require('snacks').bufdelete()<cr>", opts("Delete current buffer"))
+map('n', "<leader>bo", "<cmd>lua require('snacks').bufdelete.other()<cr>", opts("Delete other buffers"))
+map('n', "<leader>ba", "<cmd>lua require('snacks').bufdelete.all()<cr>", opts("Delete all buffers"))
+
+-- ===========================================================================
+-- NOTE: Stay in indent mode
+-- ===========================================================================
+map('v', '<', "<gv", opts("Unindent"))
+map('v', '>', ">gv", opts("Indent"))
+
+-- ===========================================================================
+-- NOTE: Move lines
+-- ===========================================================================
+map('n', "<A-j>", ":m .+1<cr>==", opts("Move line down"))
+map('n', "<A-k>", ":m .-2<cr>==", opts("Move line up"))
+map('v', "<A-j>", ":m '>+1<cr>gv=gv", opts("Move selection down"))
+map('v', "<A-k>", ":m '<-2<cr>gv=gv", opts("Move selection up"))
+
+-- ===========================================================================
+-- NOTE: Debugging
+-- ===========================================================================
+map('n', "<leader>db", "<cmd>lua require('dap').toggle_breakpoint()<cr>", opts("Debugger toggle breakpoint"))
+map('n', "<leader>dc", "<cmd>lua require('dap').continue()<cr>", opts("Debugger continue"))
+map('n', "<leader>dj", "<cmd>lua require('dap').step_over()<cr>", opts("Debugger step over"))
+map('n', "<leader>dk", "<cmd>lua require('dap').step_out()<cr>", opts("Debugger step out"))
+map('n', "<leader>dl", "<cmd>lua require('dap').step_into()<cr>", opts("Debugger step into"))
+map({ 'n', 't' }, "<leader>dm", "<cmd>lua require('snacks').terminal.open({'make'}, { win = { position = 'bottom' } })<cr>", opts("Debugger run C make"))
+map('n', "<leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<cr>", opts("Debugger run rust tests"))
+map('n', "<leader>dx", "<cmd>lua require('dap').terminate()<cr><cmd>lua require('dapui').close()<cr>", opts("Debugger terminate"))
+
+-- ===========================================================================
+-- NOTE: Git
+-- ===========================================================================
+map('n', "<leader>gb", "<cmd>lua require('snacks').git.blame_line()<cr>", opts("Git blame Line"))
+map('n', "<leader>gB", "<cmd>lua require('snacks').picker.git_branches()<cr>", opts("Git branches"))
+map('n', "<leader>gd", "<cmd>lua require('snacks').picker.git_diff()<cr>", opts("Git differences"))
+map('n', "<leader>gg", "<cmd>lua require('snacks').lazygit()<cr>", opts("Lazygit"))
+map('n', "<leader>gl", "<cmd>lua require('snacks').lazygit.log()<cr>", opts("Lazygit logs"))
+map('n', "<leader>gs", "<cmd>lua require('snacks').picker.git_status()<cr>", opts("Git status"))
+
+-- ===========================================================================
+-- NOTE: Navigation
+-- ===========================================================================
 
 -- NOTE: Window navigation
 map('n', "<C-d>", "<C-d>zz", { desc = "Go down half page and center" })
@@ -45,42 +103,6 @@ map('t', "<C-j>", [[<C-\><C-n><C-w>j]], opts("Go to lower terminal"))
 map('t', "<C-k>", [[<C-\><C-n><C-w>k]], opts("Go to upper terminal"))
 map('t', "<C-l>", [[<C-\><C-n><C-w>l]], opts("Go to right terminal"))
 
--- NOTE: Buffer navigation
-map('n', "<Tab>", "<cmd>bnext<cr>", opts("Next buffer"))
-map('n', "<S-Tab>", "<cmd>bprevious<cr>", opts("Previous buffer"))
-map('n', "<leader>bd", "<cmd>lua require('snacks').bufdelete()<cr>", opts("Delete current buffer"))
-map('n', "<leader>bo", "<cmd>lua require('snacks').bufdelete.other()<cr>", opts("Delete other buffers"))
-map('n', "<leader>ba", "<cmd>lua require('snacks').bufdelete.all()<cr>", opts("Delete all buffers"))
-
--- NOTE: Stay in indent mode
-map('v', '<', "<gv", opts("Unindent"))
-map('v', '>', ">gv", opts("Indent"))
-
--- NOTE: Move lines
-map('n', "<A-j>", ":m .+1<cr>==", opts("Move line down"))
-map('n', "<A-k>", ":m .-2<cr>==", opts("Move line up"))
-map('v', "<A-j>", ":m '>+1<cr>gv=gv", opts("Move selection down"))
-map('v', "<A-k>", ":m '<-2<cr>gv=gv", opts("Move selection up"))
-
--- NOTE: Debugging
-map('n', "<leader>db", "<cmd>lua require('dap').toggle_breakpoint()<cr>", opts("Debugger toggle breakpoint"))
-map('n', "<leader>dc", "<cmd>lua require('dap').continue()<cr>", opts("Debugger continue"))
-map('n', "<leader>dj", "<cmd>lua require('dap').step_over()<cr>", opts("Debugger step over"))
-map('n', "<leader>dk", "<cmd>lua require('dap').step_out()<cr>", opts("Debugger step out"))
-map('n', "<leader>dl", "<cmd>lua require('dap').step_into()<cr>", opts("Debugger step into"))
-map({ 'n', 't' }, "<leader>dm", "<cmd>lua require('snacks').terminal.open({'make'}, { win = { position = 'bottom' } })<cr>", opts("Debugger run C make"))
-map('n', "<leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<cr>", opts("Debugger run rust tests"))
-map('n', "<leader>dx", "<cmd>lua require('dap').terminate()<cr><cmd>lua require('dapui').close()<cr>", opts("Debugger terminate"))
-
--- NOTE: Git
-map('n', "<leader>gb", "<cmd>lua require('snacks').git.blame_line()<cr>", opts("Git blame Line"))
-map('n', "<leader>gB", "<cmd>lua require('snacks').picker.git_branches()<cr>", opts("Git branches"))
-map('n', "<leader>gd", "<cmd>lua require('snacks').picker.git_diff()<cr>", opts("Git differences"))
-map('n', "<leader>gg", "<cmd>lua require('snacks').lazygit()<cr>", opts("Lazygit"))
-map('n', "<leader>gl", "<cmd>lua require('snacks').lazygit.log()<cr>", opts("Lazygit logs"))
-map('n', "<leader>gs", "<cmd>lua require('snacks').picker.git_status()<cr>", opts("Git status"))
-
--- NOTE: Navigation
 -- NOTE: New file
 map('n', "<leader>nf", "<cmd>enew<cr>", opts("New file"))
 
@@ -108,7 +130,11 @@ map({ 'n', 't' }, "<leader>ntk", function () toggle_snacks_term("top") end, opts
 map({ 'n', 't' }, "<leader>nth", function () toggle_snacks_term("left") end, opts("Toggle terminal at left"))
 map({ 'n', 't' }, "<leader>ntl", function () toggle_snacks_term("right") end, opts("Toggle terminal at right"))
 
--- NOTE: Picker find
+-- ===========================================================================
+-- NOTE: Picker
+-- ===========================================================================
+
+-- NOTE: Find
 map('n', "<leader>fa", "<cmd>lua require('snacks').picker.autocmds({ layout = 'vertical' })<cr>", opts("Find autocmds"))
 map('n', "<leader>fb", "<cmd>lua require('snacks').picker.buffers({ focus = 'list' })<cr>", opts("Find buffers"))
 map('n', "<leader>fB", "<cmd>lua require('snacks').picker.grep_buffers()<cr>", opts("Grep open buffers"))
@@ -130,7 +156,7 @@ map('n', "<leader>fS", "<cmd>lua require('snacks').picker.colorschemes({ layout 
 map('n', "<leader>ft", "<cmd>lua require('snacks').picker.todo_comments()<cr>", opts("Find Todo comments"))
 map('n', "<leader>fT", "<cmd>lua require('snacks').picker.todo_comments({ keywords = { 'TODO', 'FIX', 'FIXME' } })<cr>", opts("Find Todo/Fix/Fixme"))
 
--- NOTE: Picker search
+-- NOTE: Search
 map('n', '<leader>s"', "<cmd>lua require('snacks').picker.registers({ layout = 'dropdown', focus = 'list' })<cr>", opts("Search registers"))
 map('n', "<leader>s:", "<cmd>lua require('snacks').picker.command_history({ focus = 'list' })<cr>", opts("Search command history"))
 map('n', "<leader>s/", "<cmd>lua require('snacks').picker.search_history({ focus = 'list' })<cr>", opts("Search history"))
@@ -139,7 +165,9 @@ map('n', "<leader>sn", "<cmd>lua require('snacks').notifier.show_history()<cr>",
 map('n', "<leader>su", "<cmd>lua require('snacks').picker.undo({ layout = 'vertical' })<cr>", opts("Search undo history"))
 map('n', "<leader>sq", "<cmd>lua require('snacks').picker.qflist()<cr>", opts("Quickfix list"))
 
+-- ===========================================================================
 -- NOTE: Words & LSP
+-- ===========================================================================
 map('n', "<leader>ld", "<cmd>lua require('snacks').picker.diagnostics()<cr>", opts("Find LSP diagnostics"))
 map('n', "<leader>lD", "<cmd>lua require('snacks').picker.diagnostics_buffer()<cr>", opts("Find LSP buffer diagnostics"))
 map('n', "<leader>ll", "<cmd>lua require('snacks').words.jump(vim.v.count1)<cr>", opts("Next LSP reference"))
@@ -153,7 +181,10 @@ map('n', "<leader>lCa", "<cmd>RainbowAlign<cr>", opts("Rainbow CSV align"))
 map('n', "<leader>lCs", "<cmd>RainbowShrink<cr>", opts("Rainbow CSV shrink"))
 map('n', "<leader>lK", "<cmd>lua require('codedocs').insert_docs()<cr>", opts("Insert code docs"))
 
+-- ===========================================================================
 -- NOTE: Folds
+-- ===========================================================================
+
 -- HACK: Function to fold all headings of a specific level
 local function fold_headings_of_level(level)
   -- Move to the top of the file
