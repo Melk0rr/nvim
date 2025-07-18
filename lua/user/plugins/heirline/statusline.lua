106e2eb9a9d5052337300c87c17656dee493efc7
Me1k0r <runner-me1k0r@proton.me>
2025-07-18 16:24:51 +0200

    edit: plugins/heirline - search count component

--- lua/user/plugins/heirline/statusline.lua
+++ lua/user/plugins/heirline/statusline.lua
@@ -419,6 +419,26 @@ local Ruler = {
 }
 
 -- ===========================================================================
+-- INFO: SearchCount
+-- ===========================================================================
+local SearchCount = {
+  condition = function()
+    return vim.v.hlsearch ~= 0 and vim.o.cmdheight == 0
+  end,
+  init = function(self)
+    local ok, search = pcall(vim.fn.searchcount)
+    if ok and search.total then
+      self.search = search
+    end
+  end,
+  provider = function(self)
+    local search = self.search
+    return string.format(" %d/%d", search.current, math.min(search.total, search.maxcount))
+  end,
+  hl = { fg = "purple", bold = true },
+}
+
+-- ===========================================================================
 -- INFO: Wrap up default status line
 -- ===========================================================================
 
