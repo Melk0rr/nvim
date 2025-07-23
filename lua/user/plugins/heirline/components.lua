0c841ab768151bc5baae7113321c138f62c880a3
Me1k0r <runner-me1k0r@proton.me>
2025-07-23 10:00:04 +0200

    test: plugins/heirline - mode component transparent bg

--- lua/user/plugins/heirline/components.lua
+++ lua/user/plugins/heirline/components.lua
@@ -59,7 +59,7 @@ local Mode = {
   end,
   hl = function(self)
     local color = self:mode_color()
-    return { fg = "none", bg = color, bold = true }
+    return { fg = "transparent", bg = color, bold = true }
   end,
   update = {
     "ModeChanged",
