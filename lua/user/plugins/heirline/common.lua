5e7104ddbf8637bf5023ff3894a4597a5c404f17
Me1k0r <runner-me1k0r@proton.me>
2025-07-23 11:01:10 +0200

    edit: plugins/heirline - blend + dim functions

--- lua/user/plugins/heirline/common.lua
+++ lua/user/plugins/heirline/common.lua
@@ -1,3 +1,26 @@
+--- Blend two rgb colors using alpha
+---@param color1 string | number first color
+---@param color2 string | number second color
+---@param alpha number (0, 1) float determining the weighted average
+---@return string color hex string of the blended color
+local function blend(color1, color2, alpha)
+  color1 = type(color1) == "number" and string.format("#%06x", color1) or color1
+  color2 = type(color2) == "number" and string.format("#%06x", color2) or color2
+  local r1, g1, b1 = color1:match("#(%x%x)(%x%x)(%x%x)")
+  local r2, g2, b2 = color2:match("#(%x%x)(%x%x)(%x%x)")
+  local r = tonumber(r1, 16) * alpha + tonumber(r2, 16) * (1 - alpha)
+  local g = tonumber(g1, 16) * alpha + tonumber(g2, 16) * (1 - alpha)
+  local b = tonumber(b1, 16) * alpha + tonumber(b2, 16) * (1 - alpha)
+  return "#"
+      .. string.format("%02x", math.min(255, math.max(r, 0)))
+      .. string.format("%02x", math.min(255, math.max(g, 0)))
+      .. string.format("%02x", math.min(255, math.max(b, 0)))
+end
+
+local function dim(color, n)
+  return blend(color, "#000000", n)
+end
+
 local icons = {
   -- ✗   󰅖 󰅘 󰅚 󰅙 󱎘 
   close = "󰅙 ",
@@ -40,5 +63,4 @@ local separators = {
   dotted_vert = "┊",
 }
 
-return { separators = separators, icons = icons }
-
+return { separators = separators, icons = icons, dim = dim }
