1c0eb6ae55147691f76659e548661c3571b2090a
Me1k0r <runner-me1k0r@proton.me>
2025-07-28 17:14:21 +0200

    edit: plugins/heirline - rolledback utf-8 display

--- lua/user/plugins/heirline/components.lua
+++ lua/user/plugins/heirline/components.lua
@@ -308,7 +308,8 @@ local FileType = {
 
 local FileEncoding = {
   provider = function()
-    return (vim.bo.fenc ~= "" and vim.bo.fenc) or vim.o.enc -- :h 'enc'
+    local enc = (vim.bo.fenc ~= "" and vim.bo.fenc) or vim.o.enc -- :h 'enc'
+    return enc ~= "utf-8" and enc:upper()
   end,
 }
 
