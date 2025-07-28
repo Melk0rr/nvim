9ad6f2c3ea4df878b50a6ffe304451c6bd40d4bf
Me1k0r <runner-me1k0r@proton.me>
2025-07-28 16:55:00 +0200

    edit: plugins/heirline - moved utils functions to dedicated file

---
+++ lua/user/plugins/heirline/color_utils.lua
@@ -0,0 +1,34 @@
+local utils = require("heirline.utils")
+
+--- Blend two rgb colors using alpha
+---@return table style style elements based on the file type
+local function file_style()
+  local filename = vim.api.nvim_buf_get_name(0)
+  local extension = vim.fn.fnamemodify(filename, ":e")
+  local icon, icon_color = require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+
+  return { filename = filename, icon = icon, icon_color = icon_color }
+end
+
+--- Blend two rgb colors using alpha
+---@param color1 string | number first color
+---@param color2 string | number second color
+---@param alpha number (0, 1) float determining the weighted average
+---@return string color hex string of the blended color
+local function blend(color1, color2, alpha)
+  color1 = type(color1) == "number" and string.format("#%06x", color1) or color1
+  color2 = type(color2) == "number" and string.format("#%06x", color2) or color2
+  local r1, g1, b1 = color1:match("#(%x%x)(%x%x)(%x%x)")
+  local r2, g2, b2 = color2:match("#(%x%x)(%x%x)(%x%x)")
+  local r = tonumber(r1, 16) * alpha + tonumber(r2, 16) * (1 - alpha)
+  local g = tonumber(g1, 16) * alpha + tonumber(g2, 16) * (1 - alpha)
+  local b = tonumber(b1, 16) * alpha + tonumber(b2, 16) * (1 - alpha)
+  return "#"
+      .. string.format("%02x", math.min(255, math.max(r, 0)))
+      .. string.format("%02x", math.min(255, math.max(g, 0)))
+      .. string.format("%02x", math.min(255, math.max(b, 0)))
+end
+
+local function dim(color, n)
+  return blend(color, "#000000", n)
+end
