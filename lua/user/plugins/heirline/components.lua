196b2a5c6f0aae04e4eba9266ec665354951cd61
Me1k0r <runner-me1k0r@proton.me>
2025-07-28 17:37:04 +0200

    edit: plugins/heirline - FileEncoding utf8 case handled in condition

--- lua/user/plugins/heirline/components.lua
+++ lua/user/plugins/heirline/components.lua
@@ -308,9 +308,11 @@ local FileType = {
 }
 
 local FileEncoding = {
+  condition = function ()
+    return file_enc() ~= "utf-8"
+  end,
   provider = function()
-    local enc = (vim.bo.fenc ~= "" and vim.bo.fenc) or vim.o.enc -- :h 'enc'
-    return enc ~= "utf-8" and enc:upper()
+    return file_enc():upper()
   end,
 }
 
