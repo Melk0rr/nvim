e51d5a272db6769acbb121c91f30ce2da6a58e4e
Me1k0r <runner-me1k0r@proton.me>
2025-07-29 19:34:46 +0200

    fix: mappings - snacks term mapping function without vim.cmd

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -145,7 +145,7 @@ map({ 'n', 'v' }, "<leader>ny", "<cmd>Yazi cwd<cr>", opts("Open Yazi in current 
 
 -- NOTE: Terminal
 local function toggle_snacks_term(pos)
-  vim.cmd([[lua require('snacks').terminal.toggle(nil, { win = { position = pos } })]])
+  require("snacks").terminal.toggle(nil, { win = { position = pos } })
 end
 
 map({ 'n', 't' }, "<leader>ntj", function() toggle_snacks_term("bottom") end, opts("Toggle terminal at bottom"))
