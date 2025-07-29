eea25957cc50a49edddb93cf725c8be5f4e78cc0
Me1k0r <runner-me1k0r@proton.me>
2025-07-29 17:38:51 +0200

    edit: plugins/heirline - tabline and winbar components into dedicated files

--- lua/user/plugins/heirline/tabline.lua
+++ lua/user/plugins/heirline/tabline.lua
@@ -1,12 +1,220 @@
 local utils = require("heirline.utils")
+
 local cmp = require("user.plugins.heirline.components")
+local sep = require("user.plugins.heirline.common").separators
+local icons = require("user.plugins.heirline.common").icons
+
+-- ===========================================================================
+-- INFO: TabLine
+-- ===========================================================================
+local TablineBufnr = {
+  provider = function(self)
+    return tostring(self.bufnr) .. ". "
+  end,
+  hl = "Comment",
+}
+
+-- we redefine the filename component, as we probably only want the tail and not the relative path
+local TablineFileName = {
+  provider = function(self)
+    -- self.filename will be defined later, just keep looking at the example!
+    local filename = self.filename
+    filename = filename == "" and "[No Name]" or vim.fn.fnamemodify(filename, ":t")
+    return filename
+  end,
+  hl = function(self)
+    return { bold = self.is_active or self.is_visible, italic = true }
+  end,
+}
+
+local TablineFileFlags = {
+  {
+    condition = function(self)
+      return vim.api.nvim_get_option_value("modified", { buf = self.bufnr })
+    end,
+    provider = "[+]",
+    hl = { fg = "green" },
+  },
+  {
+    condition = function(self)
+      return not vim.api.nvim_get_option_value("modifiable", { buf = self.bufnr })
+          or vim.api.nvim_get_option_value("readonly", { buf = self.bufnr })
+    end,
+    provider = function(self)
+      if vim.api.nvim_get_option_value("buftype", { buf = self.bufnr }) == "terminal" then
+        return "  "
+      else
+        return ""
+      end
+    end,
+    hl = { fg = "orange" },
+  },
+}
+
+local TablineFileNameBlock = {
+  init = function(self)
+    self.filename = vim.api.nvim_buf_get_name(self.bufnr)
+  end,
+  hl = function(self)
+    if self.is_active then
+      return "TabLineSel"
+      -- why not?
+      -- elseif not vim.api.nvim_buf_is_loaded(self.bufnr) then
+      --     return { fg = "gray" }
+    else
+      return "TabLine"
+    end
+  end,
+  on_click = {
+    callback = function(_, minwid, _, button)
+      if (button == "m") then -- close on mouse middle click
+        vim.schedule(function()
+          vim.api.nvim_buf_delete(minwid, { force = false })
+        end)
+      else
+        vim.api.nvim_win_set_buf(0, minwid)
+      end
+    end,
+    minwid = function(self)
+      return self.bufnr
+    end,
+    name = "heirline_tabline_buffer_callback",
+  },
+  TablineBufnr,
+  cmp.FileIcon,
+  TablineFileName,
+  TablineFileFlags,
+}
+
+local TablineCloseButton = {
+  condition = function(self)
+    return not vim.api.nvim_get_option_value("modified", { buf = self.bufnr })
+  end,
+  { provider = " " },
+  {
+    provider = "",
+    hl = { fg = "gray" },
+    on_click = {
+      callback = function(_, minwid)
+        vim.schedule(function()
+          vim.api.nvim_buf_delete(minwid, { force = false })
+          vim.cmd.redrawtabline()
+        end)
+      end,
+      minwid = function(self)
+        return self.bufnr
+      end,
+      name = "heirline_tabline_close_buffer_callback",
+    },
+  },
+}
+
+local TablinePicker = {
+  condition = function(self)
+    return self._show_picker
+  end,
+  init = function(self)
+    local bufname = vim.api.nvim_buf_get_name(self.bufnr)
+    bufname = vim.fn.fnamemodify(bufname, ":t")
+    local label = bufname:sub(1, 1)
+    local i = 2
+    while self._picker_labels[label] do
+      label = bufname:sub(i, i)
+      if i > #bufname then
+        break
+      end
+      i = i + 1
+    end
+    self._picker_labels[label] = self.bufnr
+    self.label = label
+  end,
+  provider = function(self)
+    return self.label
+  end,
+  hl = { fg = "red", bold = true },
+}
+
+local TablineBufferBlock = utils.surround({ sep.slant_left, sep.slant_right }, function(self)
+  if self.is_active then
+    return utils.get_highlight("TabLineSel").bg
+  else
+    return utils.get_highlight("TabLine").bg
+  end
+end, { TablinePicker, TablineFileNameBlock, TablineCloseButton })
+
+local Tabpage = {
+  {
+    provider = function(self)
+      return " %" .. self.tabnr .. "T" .. self.tabnr .. " "
+    end,
+    hl = { bold = true },
+  },
+  {
+    provider = function(self)
+      local n = #vim.api.nvim_tabpage_list_wins(self.tabpage)
+      return n .. "%T "
+    end,
+    hl = { fg = "gray" },
+  },
+  hl = function(self)
+    if not self.is_active then
+      return "TabLine"
+    else
+      return "TabLineSel"
+    end
+  end,
+  update = { "TabNew", "TabClosed", "TabEnter", "TabLeave", "WinNew", "WinClosed" },
+}
+
+local TabpageClose = {
+  provider = " %999X" .. icons.close .. "%X",
+  hl = "TabLine",
+}
+
+local TabPages = {
+  condition = function()
+    return #vim.api.nvim_list_tabpages() >= 2
+  end,
+  cmp.Align,
+  utils.make_tablist(Tabpage),
+  TabpageClose,
+}
+
+local TabLineOffset = {
+  condition = function(self)
+    local win = vim.api.nvim_tabpage_list_wins(0)[1]
+    local bufnr = vim.api.nvim_win_get_buf(win)
+    self.winid = win
+
+    local offset_type = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+    if offset_type == "neo-tree" or offset_type == "snacks_layout_box" then
+      self.title = "󰙅  Files"
+      return true
+    end
+  end,
+  provider = function(self)
+    local title = self.title
+    local width = vim.api.nvim_win_get_width(self.winid)
+    local pad = math.ceil((width - #title) / 2)
+    return string.rep(" ", pad) .. title .. string.rep(" ", pad)
+  end,
+  hl = function(self)
+    if vim.api.nvim_get_current_win() == self.winid then
+      return "TablineSel"
+    else
+      return "Tabline"
+    end
+  end,
+}
 
 local BufferLine = utils.make_buflist(
-    cmp.TablineBufferBlock,
-    { provider = "", hl = { fg = "gray" } },
-    { provider = "", hl = { fg = "gray" } }
+  TablineBufferBlock,
+  { provider = "", hl = { fg = "gray" } },
+  { provider = "", hl = { fg = "gray" } }
 )
 
 return {
-  BufferLine
+  TabLineOffset,
+  BufferLine,
+  TabPages,
 }
--- lua/user/plugins/heirline/winbar.lua
+++ lua/user/plugins/heirline/winbar.lua
@@ -1,8 +1,95 @@
-local cmp = require("user.plugins.heirline.components")
+local utils = require("heirline.utils")
 
-local WinBar = {
-  cmp.Navic,
-  cmp.Align
+local cmp = require("user.plugins.heirline.components")
+local dim = require("user.plugins.heirline.heirline_utils")
+
+-- ===========================================================================
+-- INFO: Navic
+-- ===========================================================================
+local Navic = {
+  condition = function()
+    return require("nvim-navic").is_available()
+  end,
+  static = {
+    type_hl = {
+      File = dim(utils.get_highlight("Directory").fg, 75),
+      Module = dim(utils.get_highlight("@module").fg, .75),
+      Namespace = dim(utils.get_highlight("@module").fg, .75),
+      Package = dim(utils.get_highlight("@module").fg, .75),
+      Class = dim(utils.get_highlight("@type").fg, .75),
+      Method = dim(utils.get_highlight("@function.method").fg, .75),
+      Property = dim(utils.get_highlight("@property").fg, .75),
+      Field = dim(utils.get_highlight("@variable.member").fg, .75),
+      Constructor = dim(utils.get_highlight("@constructor").fg, .75),
+      Enum = dim(utils.get_highlight("@type").fg, .75),
+      Interface = dim(utils.get_highlight("@type").fg, .75),
+      Function = dim(utils.get_highlight("@function").fg, .75),
+      Variable = dim(utils.get_highlight("@variable").fg, .75),
+      Constant = dim(utils.get_highlight("@constant").fg, .75),
+      String = dim(utils.get_highlight("@string").fg, .75),
+      Number = dim(utils.get_highlight("@number").fg, .75),
+      Boolean = dim(utils.get_highlight("@boolean").fg, .75),
+      Array = dim(utils.get_highlight("@variable.member").fg, .75),
+      Object = dim(utils.get_highlight("@type").fg, .75),
+      Key = dim(utils.get_highlight("@keyword").fg, .75),
+      Null = dim(utils.get_highlight("@comment").fg, .75),
+      EnumMember = dim(utils.get_highlight("@constant").fg, .75),
+      Struct = dim(utils.get_highlight("@type").fg, .75),
+      Event = dim(utils.get_highlight("@type").fg, .75),
+      Operator = dim(utils.get_highlight("@operator").fg, .75),
+      TypeParameter = dim(utils.get_highlight("@type").fg, .75),
+    },
+    -- line: 16 bit (65536); col: 10 bit (1024); winnr: 6 bit (64)
+    -- local encdec = function(a,b,c) return dec(enc(a,b,c)) end; vim.pretty_print(encdec(2^16 - 1, 2^10 - 1, 2^6 - 1))
+    enc = function(line, col, winnr)
+      return bit.bor(bit.lshift(line, 16), bit.lshift(col, 6), winnr)
+    end,
+    dec = function(c)
+      local line = bit.rshift(c, 16)
+      local col = bit.band(bit.rshift(c, 6), 1023)
+      local winnr = bit.band(c, 63)
+      return line, col, winnr
+    end,
+  },
+  init = function(self)
+    local data = require("nvim-navic").get_data() or {}
+    local children = {}
+    for i, d in ipairs(data) do
+      local pos = self.enc(d.scope.start.line, d.scope.start.character, self.winnr)
+      local child = {
+        {
+          provider = d.icon,
+          hl = { fg = self.type_hl[d.type] },
+        },
+        {
+          provider = d.name:gsub("%%", "%%%%"):gsub("%s*->%s*", ""),
+          hl = { fg = self.type_hl[d.type] },
+          -- hl = self.type_hl[d.type],
+          on_click = {
+            callback = function(_, minwid)
+              local line, col, winnr = self.dec(minwid)
+              vim.api.nvim_win_set_cursor(vim.fn.win_getid(winnr), { line, col })
+            end,
+            minwid = pos,
+            name = "heirline_navic",
+          },
+        },
+      }
+      if i < #data then
+        table.insert(child, {
+          provider = " → ",
+          hl = { fg = "bright_fg" },
+        })
+      end
+      table.insert(children, child)
+    end
+    self[1] = self:new(children, 1)
+  end,
+  update = "CursorMoved",
+  hl = { fg = "gray" },
 }
 
-return WinBar
+return {
+  Navic,
+  cmp.Align
+}
